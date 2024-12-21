c93b6cadbef15644de433ea562b1f61e32861a9e
DevNewbieJW <julian.weusten@fondof.de>
2024-12-21 19:08:52 +0100

    new user form

--- app/routes/users.new.tsx
+++ app/routes/users.new.tsx
@@ -1,9 +1,113 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Input } from "@/components/ui/input";
+
+import { Link, createFileRoute } from "@tanstack/react-router";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 
 export const Route = createFileRoute("/users/new")({
   component: RouteComponent,
 });
 
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .min(2, {
+      message: "email must be at least 2 characters.",
+    })
+    .email(),
+  description: z.string().optional(),
+});
+
 function RouteComponent() {
-  return <div>Hello "/users/new"!</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      description: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-screen-lg"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="name of your contact" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>E-Mail</FormLabel>
+              <FormControl>
+                <Input placeholder="the contacts email..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Descriptionm</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="write something about this contact"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex space-x-2">
+          <Button type="submit">Submit</Button>
+          <Link
+            to="/"
+            className={cn(buttonVariants({ variant: "destructive" }))}
+          >
+            Cancel
+          </Link>
+        </div>
+      </form>
+    </Form>
+  );
 }
